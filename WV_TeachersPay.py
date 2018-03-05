c9b02b6e0f29ec02d319869b1a1ee992f99384ce
sachinmyneni <30158159+sachinmyneni@users.noreply.github.com>
2018-03-05 12:12:12 -0800

    wsBcBAABCAAQBQJanaScCRBK7hj4Ov3rIwAAdHIIAGipDQgsOlXBF2SGai2MC5Bg  6bIS7edhKKJq1KbIeoKODP7HWaHCIxHWJ+Xz/3afaY/zypHVAOl29WW9TGtbP12L  14tTt0YQVQQK8dcx3iysWhp1xd/G4dS1RP5p+27oTp7DhPfkWzNrw+WpjBvbhe7v  9zZvO2y1Ndjo2yTdEjAjGYpSmlZVPsnzO5k8+Lbm5974HcteASzIR6a+DBkDGycP  ikqefwHeWcmkoUrvUHPsqUADtMCMpPQokxyx6UhV2hrj8/5feWGK6etajjaQxc2i  GgQJKsSgDQ912TD7Pz0CMIBE3APsQ2pbdAuPTbAFpxFfwWS/IXiKqZ6K3qPvDwA=  =LsPM  -----END PGP SIGNATURE-----
    added more stats

--- WV_TeachersPay.py
+++ WV_TeachersPay.py
@@ -1,2 +1,3 @@
 df = pd.read_excel('state_M2016_df.xlsx',header=0,encoding=sys.getfilesystemencoding())
 df.loc[df.OCC_CODE=='25-2022',['STATE','TOT_EMP','A_MEAN']].sort('A_MEAN').reset_index(drop=True)
+df.loc[df.OCC_CODE=='25-2011',['STATE','A_MEAN','A_MEDIAN','A_PCT10','A_PCT25','A_PCT75','A_PCT90']].sort('A_MEDIAN').reset_index(drop=True).plot()
